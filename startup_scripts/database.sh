70ab9784dae128e09e80f68f60eee7f0ded7db98
rp54 <99834093+Ritikp1@users.noreply.github.com>
2023-10-09 20:20:47 +0000

    Updates database.sh file
    Added prompt to open MySQL

--- startup_scripts/database.sh
+++ startup_scripts/database.sh
@@ -8,3 +8,11 @@ else
 	echo "Starting MySQL"
 	systemctl start mysql.service
 fi
+
+echo "Do you want to open MySQL? [Y/N]"
+read n
+typeset -l n
+if [[ $n = "y" ]]; then
+	echo "Opening MySQl"
+	sudo mysql --user=root
+fi
