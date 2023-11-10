3c0b3a3ffdac1577339bbd17907cdf7395831aa2
ag884 <144181511+ag884@users.noreply.github.com>
2023-11-10 04:26:29 +0000

    Add files via upload
    Most up to date rabbitmq consumer script

--- Backend Scripts/receiver.py
+++ Backend Scripts/receiver.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import json
+from shapeshift.models import User
+
 try:
     import pika
 except Exception as e:
@@ -10,13 +13,21 @@ connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='10.248.179.6', credentials = credentials))
 
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='front-back', durable=True)
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    data = json.loads(body)
+    user = User(
+        first_name=data['first'],
+        last_name=data['last'],
+        email=data['email'] ,
+        password=data['psw'] 
+    )
+    user.save()
+    print("Received data:", data)
 
 channel.basic_consume(
-        queue='hello', on_message_callback=callback, auto_ack=True)
+        queue='front-back', on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
