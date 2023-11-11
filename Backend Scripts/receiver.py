89dafae22260d14f64e957277b74d014be20f82e
ag884 <144181511+ag884@users.noreply.github.com>
2023-11-11 21:49:46 +0000

    Add files via upload

--- Backend Scripts/receiver.py
+++ Backend Scripts/receiver.py
@@ -6,11 +6,11 @@ import pika
 import sys
 import json
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IT490.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project490.settings')
 credentials = pika.PlainCredentials('backend','password')
 parameters = pika.ConnectionParameters(
-    host='10.248.179.10', 
-    port=5672, 
+    host='10.248.179.10',
+    port=5672,
     credentials = credentials
  )
 
@@ -19,9 +19,8 @@ try:
 
 except Exception as e:
     print("Some modules are missing {}".format(e))
-    sys.exit(1)
 
-from users.models import CustomUser
+#from users.models import CustomUser
 from users.utils import save_user_info
 
 connection = pika.BlockingConnection(parameters)
@@ -31,18 +30,28 @@ channel.queue_declare(queue='front-back', durable=True)
 
 def callback(ch, method, properties, body):
     data = json.loads(body.decode('utf-8'))
-    first_name = data.get('first')
-    last_name = data.get('last')
-    email = data.get('email')
-    password = data.get('psw')
     print("Received data:", data)
-    save_user_info(first_name, last_name, email, password)
+    try:
+        first_name = data.get('first')
+        last_name = data.get('last')
+        email = data.get('email')
+        password = data.get('password')
+        print("Processing data:", first_name, last_name, email)
+        save_user_info(first_name, last_name, email, password)
+    except KeyError as e:
+        print(f"Error processing message: {e}")
+
 
 channel.basic_consume(
         queue='front-back', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+try:
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+except KeyboardInterrupt:
+    print(' [*] Exiting due to user interruption')
+except Exception as e:
+    print(f"Error during message consumption: {e}")
 
 
 
