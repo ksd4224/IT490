dbad2e5e1c8df6c39fe3d73ae0a020bc22c0c6dc
ksd4224 <76744692+ksd4224@users.noreply.github.com>
2023-11-13 16:35:08 -0500

    Update index.php

--- Frontend/index.php
+++ Frontend/index.php
@@ -1,16 +1,26 @@
 <?php
         session_start();
-	$_SESSION['first'] = $_POST['first'];
-	$_SESSION['last'] = $_POST['last'];
-	$_SESSION['email'] = $_POST['email'];
-        $_SESSION['psw'] = $_POST['psw'];
+        $_SESSION['first'] = $_POST['first'];
+        $_SESSION['last'] = $_POST['last'];
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['password'] = $_POST['password'];
         $email = $_SESSION['email'];
 ?>
 
 <html>
-	<head>
+        <head>
                 <title> ShapeShift </title>
                 <link rel="stylesheet" href="index.css" />
+                <script>
+                        function validate(){
+                                var a = document.getElementById("password").value;
+                                var b = document.getElementById("password-repeat").value;
+                                if (a!=b) {
+                                        alert("Passwords do no match");
+                                        return false;
+                                }       
+                        }
+                </script>
         </head>
 
         <body style="color:#008080;" >
@@ -21,7 +31,7 @@
                         <h1 class="register"> REGISTRATION </h1>
                         <p align="center">Already have an account?<a href="/login.php"> Login here </a></p>
                 </h1>
-                <form action="/login2.php" method="post">
+                <form onsubmit="return validate();" action="/login2.php" method="post">
                         <label for="first"><b>First Name</b></label>
                         <input type="text" placeholder="Enter First Name" name="first" id="first" required>
                 <br><br>
@@ -32,11 +42,11 @@
                         <label for="email"><b>Email</b></label>
                         <input type="text" placeholder="Enter Email" name="email" id="email" required>
                 <br><br>
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+                        <label for="password"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="password" id="password" required>
                 <br><br>
-                        <label for="psw-repeat"><b>Repeat Password</b></label>
-                        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+                        <label for="password-repeat"><b>Repeat Password</b></label>
+                        <input type="password" placeholder="Repeat Password" name="password-repeat" id="password-repeat" required>
                 <br><br>
                         <button type="submit" class="registerbtn">Register</button>
                 </form>
