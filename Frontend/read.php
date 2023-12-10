1d7f5191c0d6b5f999e87eb6674afdf8a51486fe
ksd4224 <76744692+ksd4224@users.noreply.github.com>
2023-12-10 00:00:37 -0500

    Update read.php

--- Frontend/read.php
+++ Frontend/read.php
@@ -6,14 +6,14 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 session_start();
 
 // RabbitMQ server connection parameters
-$rabbitMQHost = '10.248.179.6'; //if we don’t get a static ip address this has to be changed to my ip everytime
+$rabbitMQHost = '10.147.17.79'; // Change this to your RabbitMQ server IP address
 $rabbitMQPort = 5672; // Default RabbitMQ port
-$rabbitMQUser = 'frontend';
+$rabbitMQUser = 'backend';
 $rabbitMQPassword = 'password';
 $virtualHost = '/';
 
 // Queue name (frontend-to-backend-queue)
-$queueName = 'back-front'; // Replace with the actual queue name
+$queueName = 'front-reg-response'; //back-front
 
 try {
     // Create a connection to RabbitMQ
@@ -27,26 +27,62 @@ try {
 
     echo "Waiting for messages from the backend...\n";
 
+    // Set a timeout of 30 seconds (adjust as needed)
+    $timeout = 100;
+    $start_time = time();
+    ob_start();
+
     // Callback function to process received messages
     $callback = function ($message) {
-        echo 'Received: ', $message->body, "\n";
-    };
+        // Store the received message in the session
+        $_SESSION['received_message'] = $message->body;
 
-    //store in global variable
-    $receivedMessage = $message->body;
-    $_SESSION['received_message'] = $receivedMessage;
+        echo 'Received: ', $message->body, "\n";
+
+        $decodedMessage = json_decode($message->body, true);
+
+        if ($decodedMessage !== null) {
+        // Check if the 'status' field is 'success'
+                if (isset($decodedMessage['status']) && $decodedMessage['status'] === 'success') {
+                        // Redirect the user to the home screen or any desired location
+                        header("Location: login2.php");
+                        exit(); // Ensure script termination after the header redirection
+                } else if (isset($decodedMessage['status']) && $decodedMessage['status'] === 'success') {
+                        // Redirect the user to the home screen or any desired location
+                        header("Location: index.php?error=user_exists");
+                        exit(); // Ensure script termination after the header redirection
+                }
+                else {
+                        header("Location: index.php?error=no_success");
+                        exit();
+                }
+        } else {
+                // Handle JSON decoding error (invalid JSON format)
+                header("Location: index.php?error=json_decode_error");
+                exit();
+        }
+    };
 
     // Consume messages from the queue
     $channel->basic_consume($queueName, '', false, true, false, false, $callback);
 
-    // Wait for messages
-    while (count($channel->callbacks)) {
-        $channel->wait();
+    // Wait for messages or until the timeout is reached
+    while (count($channel->callbacks) && (time() - $start_time) < $timeout) {
+        $channel->wait(null, false, $timeout);
     }
 
     $channel->close();
     $connection->close();
-    
+
+    // Check if the timeout occurred
+    if ((time() - $start_time) >= $timeout) {
+        ob_end_clean(); // Clean (erase) the output buffer
+        header("Location: index.php?error=timeout");
+        exit(); // Terminate the script
+    }
+
 } catch (\Exception $e) {
     echo "Error: " . $e->getMessage() . "\n";
 }
+
+?>
