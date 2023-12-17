ffda105f67e882e1df3003257b0041e0ccef7d9d
ksd4224 <76744692+ksd4224@users.noreply.github.com>
2023-12-17 18:06:32 -0500

    Update workout.php

--- Frontend/workout.php
+++ Frontend/workout.php
@@ -1,6 +1,6 @@
 <?php
         session_start();
-        $user_data = $_SESSION['user_data'];
+	$user_data = $_SESSION['user_data'];
         $email = $user_data['email'];
         $user_id = $user_data['user_id'];
 ?>
@@ -30,8 +30,8 @@
                                 <div class="dropdown-content">
                                         <a href="profile.php">Edit profile</a>
                                         <a href="goals.php">Edit Goals</a>
-                                        <a href="trend.php">7-day Trend</a>
-                                        <a href="meals.php">Add Meals</a>
+					<a href="trend.php">7-day Trend</a>
+					<a href="meals.php">Add Meals</a>
                                         <a href="workout.php">Add Workout</a>
                                         <a href="add_weight.php">Add Weight</a>
                                 </div>
@@ -45,61 +45,61 @@
                                 </div>
                         </div>
                         <a href="aboutus.php">About Us</a>
-                </div>
-                <div class="container">
-                        <div class="content">
-                        <h1>Exercise Tracker</h1>
-                <form id="workoutForm" action="check6.php" method="post">
-                        <h2>All Exercises</h2>
-                        <!-- Display Exercises -->
-                        <?php
-                        // Fetch exercises from the API
-                        //$exercises = json_decode(file_get_contents($apiUrl), true);
-                        $apiUrl = 'https://api.api-ninjas.com/v1/exercises';
-                        $apiKey = 'GpG1j0xD2Zjw5rN+zPlV3Q==b2pGItxJcN1RFSCH';
+		</div>
+		<div class="container">
+			<div class="content">
+			<h1>Exercise Tracker</h1>
+		<form id="workoutForm" action="check6.php" method="post">
+        		<h2>All Exercises</h2>
+        		<!-- Display Exercises -->
+        		<?php
+        		// Fetch exercises from the API
+        		//$exercises = json_decode(file_get_contents($apiUrl), true);
+			$apiUrl = 'https://api.api-ninjas.com/v1/exercises';
+			$apiKey = 'GpG1j0xD2Zjw5rN+zPlV3Q==b2pGItxJcN1RFSCH';
 
-                        $options = [
-                                'http' => [
-                                'header' => "X-Api-Key: $apiKey",
-                                ],
-                        ];
+			$options = [
+    				'http' => [
+        			'header' => "X-Api-Key: $apiKey",
+    				],
+			];
 
-                        $context = stream_context_create($options);
-                        $result = file_get_contents($apiUrl, false, $context);
-                        $exercises = json_decode($result, true);
+			$context = stream_context_create($options);
+			$result = file_get_contents($apiUrl, false, $context);
+			$exercises = json_decode($result, true);
 
-                        if ($exercises === null || empty($exercises)) {
-                                echo 'Error fetching exercises from the API.';
-                        } else{
-                        //echo '<ul>';
-                        foreach ($exercises as $exercise) {
-                                echo '</br>';
-                                echo '<div style="text-align: center; font-family: Arial, sans-serif;">';
-                                echo '<strong style="font-size: 25px">Name:</strong> <span style="font-size: 25px;">' . $exercise['name'] . '</span><br>';
-                                echo '<strong style="font-size: 25px">Type:</strong> <span style="font-size: 25px;">' . $exercise['type'] . '</span><br>';
-                                echo '<strong style="font-size: 25px">Muscle:</strong> <span style="font-size: 25px;">' . $exercise['muscle'] . '</span><br>';
-                                echo '<strong style="font-size: 25px">Equipment:</strong> <span style="font-size: 25px;">' . $exercise['equipment'] . '</span><br>';
-                                echo '<button class="button1" name="addToWorkout" type="submit" value="' . $exercise['name'] . '">Add to Workout</button>';
-                                echo '</br>';
-                                echo '</div>';
-                        }
-                        }
-                        ?>
-                </form>
-                </div>
-                        <div class="sidebar">
+			if ($exercises === null || empty($exercises)) {
+				echo 'Error fetching exercises from the API.';
+			} else{
+        		//echo '<ul>';
+			foreach ($exercises as $exercise) {
+    				echo '</br>';
+    				echo '<div style="text-align: center; font-family: Arial, sans-serif;">';
+   				echo '<strong style="font-size: 25px">Name:</strong> <span style="font-size: 25px;">' . $exercise['name'] . '</span><br>';
+    				echo '<strong style="font-size: 25px">Type:</strong> <span style="font-size: 25px;">' . $exercise['type'] . '</span><br>';
+    				echo '<strong style="font-size: 25px">Muscle:</strong> <span style="font-size: 25px;">' . $exercise['muscle'] . '</span><br>';
+    				echo '<strong style="font-size: 25px">Equipment:</strong> <span style="font-size: 25px;">' . $exercise['equipment'] . '</span><br>';
+    				echo '<button class="button1" name="addToWorkout" type="submit" value="' . $exercise['name'] . '">Add to Workout</button>';
+    				echo '</br>';
+    				echo '</div>';
+			}
+			}	
+			?>
+		</form>
+		</div>
+			<div class="sidebar">
                                 <h2> Tracking workouts is crucial for: </h2>
-                                <ul style="font-size: 25px">
-                                <li> Monitoring progress </li></br>
-                                <li> Staying motivated </li></br>
-                                <li> Identifying patterns and trends </li></br>
-                                <li> Preventing plateaus </li> </br>
-                                <li> Customizing workouts </li> </br>
-                                <li> Adding accountability </li></br>
-                                <li> Preventing injuries </li></br>
-                                <li> Improving efficiency </li></br>
-                                <li> Making data-driven decisions for optimal fitness results </li> </ul>
-                        </div>
-                </div>
-        </body>
+				<ul style="font-size: 25px">
+    				<li> Monitoring progress </li></br>
+    				<li> Staying motivated </li></br>
+    				<li> Identifying patterns and trends </li></br>
+   	 			<li> Preventing plateaus </li> </br>
+    				<li> Customizing workouts </li> </br>
+    				<li> Adding accountability </li></br>
+    				<li> Preventing injuries </li></br>
+    				<li> Improving efficiency </li></br>
+    				<li> Making data-driven decisions for optimal fitness results </li> </ul>
+			</div>
+		</div>
+	</body>
 </html>
