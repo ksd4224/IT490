1b084f4d0e44fed5f7c2d6bd38fa60be9b5fe9d9
ksd4224 <76744692+ksd4224@users.noreply.github.com>
2023-12-17 17:51:18 -0500

    Update aboutus.php

--- Frontend/aboutus.php
+++ Frontend/aboutus.php
@@ -9,6 +9,34 @@
         <head>
                 <title> ShapeShift </title>
                 <link rel="stylesheet" href="/index.css" />
+        <style>
+            .content-container {
+                background-color: #fff; /* Set the background color for the content container */
+                padding: 20px; /* Adjust padding as needed */
+                text-align: center;
+            }
+            .img4-container {
+                text-align: center;
+                margin-top: 40px; /* Adjust the top margin to add more space */
+            }
+
+            .img4 {
+                display: block;
+                margin: auto;
+                max-width: 40%;
+                height: auto;
+                border: 2px solid teal;
+            }
+
+            .creators-description {
+                text-align: center;
+                margin-top: 20px;
+                max-width: 70%;
+                margin-left: auto;
+                margin-right: auto;
+                font-size: 22px;
+            }
+        </style>
         </head>
 
         <body style="color:#008080;" >
@@ -32,6 +60,9 @@
                                         <a href="edit_profile.php">Edit profile</a>
                                         <a href="goals.php">Edit Goals</a>
                                         <a href="trend.php">Trend</a>
+                                        <a href="meals.php">Add Meals</a>
+                                        <a href="workout.php">Add Workout</a>
+                                        <a href="add_weight.php">Add Weight</a>
                                 </div>
                         </div>
                         <div class="dropdown">
@@ -43,6 +74,16 @@
                                 </div>
                         </div>
                         <a href="aboutus.php">About Us</a>
+                </div>
+                <div class="content-container">
+                <h1 style="text-align: center; margin-top: 50px;">About the Creators</h1>
+                <div class="img4-container">
+                        <img class="img4" src="us.JPG" alt="Image of the Founders">
+                </div>
+
+                 <p class="creators-description">
+                 Meet the masterminds behind the Shapeshift Application! From left to right, first we have our frontend developer Keya, her favorite workout is math; a good brain exercise! Up next we got our champion weightlifter, Ritik, who is also our database manager. In the middle we have Adam, our backend developer, whose favorite sport is to run after his child. And finally our rabbitmq manager, Nikita, who loves smores and conducted the smores trip in the middle of class. We're all excited for our users to integrate the application into their daily lives and use it to ShapeShift into their best selves!
+                 </p>
                 </div>
         </body>
 </html>
