cbc125ab77492cf52fdba452cdc11cec11583c3a
ksd4224 <76744692+ksd4224@users.noreply.github.com>
2023-12-17 18:03:47 -0500

    Update profile.php

--- Frontend/profile.php
+++ Frontend/profile.php
@@ -1,19 +1,19 @@
 <?php
         session_start();
-        //$_SESSION['email'] = $_POST['email'];
-        //$_SESSION['password'] = $_POST['password'];
-        $email = $_SESSION['email'];
-        echo "hi, " . $email;
-        //include 'write2.php';
-        //include 'read.php';
-        //if (isset($_SESSION['received_message'])){
-        //      $receivedMessage = $_SESSION['received_message'];
+        $user_data = $_SESSION['user_data'];
+        $email = $user_data['email'];
+	$height = $user_data['height'];
+	$weight = $user_data['weight'];
+	$first = $user_data['first_name'];
+	$last = $user_data['last_name'];
+	
+	if (isset($_GET['profile']) && $_GET['profile'] === 'success') {
+                echo '<script>alert("Profile Updated!");</script>';
+        }
+        else if (isset($_GET['profile']) && $_GET['profile'] === 'no_success') {
+                echo '<script>alert("Profile unable to update, please try again!");</script>';
+        }
 
-                // Now, you can use $receivedMessage in your HTML content
-        //      echo "Received Message: " . $receivedMessage;
-        //} else{
-        //      echo "No message received yet";
-        //}
 ?>
 <html>
         <head>
@@ -39,52 +39,62 @@
                                 <button class="dropbtn"> Profile &#8609;
                                 </button>
                                 <div class="dropdown-content">
-                                        <a href="#">Edit profile</a>
-                                        <a href="#">Edit Goals</a>
-                                        <a href="#">Trend</a>
+                                        <a href="profile.php">Edit profile</a>
+                                        <a href="goals.php">Edit Goals</a>
+					<a href="trend.php">7-day Trend</a>
+					<a href="meals.php">Add Meals</a>
+                                        <a href="workout.php">Add Workout</a>
+                                        <a href="add_weight.php">Add Weight</a>
                                 </div>
                         </div>
                         <div class="dropdown">
                                 <button class="dropbtn"> Community &#8609;
                                 </button>
                                 <div class="dropdown-content">
-                                        <a href="#">Community Forum</a>
-                                        <a href="#">Friends</a>
+                                        <a href="forum.php">Community Forum</a>
+                                        <a href="friends.php">Friends</a>
                                 </div>
                         </div>
-                        <a href="test.php">About Us</a>
+                        <a href="aboutus.php">About Us</a>
                 </div>
                 <div class="main" style="margin-left: 0px;width: 99%;">
-                        <h2>PROFILE</h2>
-                        <div class="card">
-                        <div class="card-body">
-                        <i class="fa fa-pen fa-xs edit"></i>
-                        <table>
-                                <tbody>
-                                        <tr>
-                                                <td style="width: 150px;">Name</td>
-                                                <td>:</td>
-                                                <td>Test</td>
-                                        </tr>
-                                        <tr>
-                                                <td>Email Address</td>
-                                                <td>:</td>
-                                                <td><?php echo $email ?> </td>
-                                        </tr>
-                                        <tr>
-                                                <td>Height</td>
-                                                <td>:</td>
-                                                <td>5ft 6in</td>
-                                        </tr>
-                                        <tr>
-                                                <td>Weight</td>
-                                                <td>:</td>
-                                                <td>140 lbs </td>
-                                        </tr>
-                                </tbody>
-                        </table>
-                        </div>
-                        </div>
-                </div>
+        		<h2>PROFILE</h2>
+        		<div class="card">
+            		<div class="card-body">
+               	 	<i class="fa fa-pen fa-xs edit"></i>
+                	<table>
+                    		<tbody>
+                        		<tr>
+                            			<td style="width: 150px;">Name</td>
+                            			<td>:</td>
+						<td><?php echo $first . " " .  $last ?></td>
+                        		</tr>
+                        		<tr>
+                            			<td>Email Address</td>
+                            			<td>:</td>
+						<td><?php echo $email ?> </td>
+                       	 		</tr>
+                        		<tr>
+                            			<td>Height</td>
+                           		 	<td>:</td>
+						<td><?php echo $height ?> in</td>
+                        		</tr>
+                        		<tr>
+						<td>Weight</td>
+						<td>:</td>
+						<td><?php echo $weight ?> lbs</td>
+                            		</tr>
+                    		</tbody>
+			</table>
+			<button class="button1" onclick="editProfile()">Edit Profile</button>
+            		</div>
+			</div>
+		</div>
+		<script>
+        		function editProfile() {
+            			// Redirect to the page where users can edit their profile
+            			window.location.href = 'edit_profile.php';
+        		}
+    		</script>
         </body>
 </html>
